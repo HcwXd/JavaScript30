6f256942169749d8d2904d3dba86e8ca26b488c1
HcwXd <e04dl47878@gmail.com>
2018-07-29 17:48:31 +0800

    Upload JS19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -23,7 +23,69 @@ function renderVideoOnCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        let pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        // pixels = redEffect(pixels);
+
+        pixels = rgbSplit(pixels);
+        ctx.globalAlpha = 0.1;
+
+        // pixels = greenScreen(pixels);
+
+        ctx.putImageData(pixels, 0, 0);
     }, 16);
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; // RED
+        pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+        pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin && green >= levels.gmin && blue >= levels.bmin && red <= levels.rmax && green <= levels.gmax && blue <= levels.bmax) {
+            // take it out!
+            pixels.data[i + 3] = 0;
+        }
+    }
+
+    return pixels;
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'screenshot');
+    link.innerHTML = `<img src="${data}" alt="Your Screenshot" />`;
+    strip.insertBefore(link, strip.firsChild);
 }
 getUserVideo();
 
