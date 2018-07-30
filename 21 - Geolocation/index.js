f71542bcc9d2840f48e96baceebda68c1d4002d4
HcwXd <e04dl47878@gmail.com>
2018-07-30 14:03:34 +0800

    Upload JS21

---
+++ 21 - Geolocation/index.js
@@ -0,0 +1,12 @@
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition(
+    (data) => {
+        speed.textContent = data.coords.speed;
+        arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+    },
+    (err) => {
+        console.error(err);
+    }
+);
